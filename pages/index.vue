639e5ee6b369affbcfc74a5484c91f32a20d4675
BMH <minhobaek@Mino-MacBook-Pro.local>
2022-10-02 23:19:12 +0900

    첫 커밋

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div>넉스트 시작하기</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
---
+++ pages/main.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>메인 페이지</div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style></style>
